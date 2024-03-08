1dae9b243fb2a029df6e7b3598665856da3e3b00
Nicolas Jaros <njaros@z2r7p2.42lyon.fr>
2024-03-08 13:02:46 +0100

    connected to db

--- back/api.py
+++ back/api.py
@@ -1,16 +1,37 @@
 from flask import Flask
 from flask_restful import Api, Resource
 from flask_cors import CORS
+from markupsafe import escape
+import psycopg2
 import os
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+conn = psycopg2.connect(dbname="users",
+                        host="db_container",
+                        user=os.environ.get("POSTGRES_USER"),
+                        password=os.environ.get("POSTGRES_PASSWORD"))
 
 
 class HelloWorld(Resource):
     def get(self):
         return "hello world"
+
+
+@app.route("/")
+def home():
+    return "you are at home"
+
+
+@app.route("/leave")
+def leave():
+    return "you leaved home"
+
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}"
 
 
 api.add_resource(HelloWorld, "/helloworld")
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { NavLink } from "react-router-dom";
 
 const ip = import.meta.env.VITE_IP;
 const Axios = axios.create({
@@ -9,6 +11,12 @@ const Axios = axios.create({
 
 const App: React.FC = () => {
 	const [hello, setHello] = useState<string>("pouet");
+	const [home, setHome] = useState<string>("not at home");
+	const [atHome, setAtHome] = useState<boolean>(false);
+	const [userName, setUserName] = useState<string>("");
+	const [buildingUserName, setBuildingUserName] = useState<string>("");
+	const [helloMsg, setHelloMsg] = useState<string>("");
+	const { handleSubmit } = useForm<{}>({});
 
 	useEffect(() => {
 		Axios.get("helloworld").then(
@@ -21,8 +29,66 @@ const App: React.FC = () => {
 			})
 	}, [])
 
+	const homing = () => {
+		if (!atHome)
+		{
+			Axios.get("").then(
+				response => {
+					setAtHome(true);
+					setHome(response.data);
+				}
+			).catch(error => {
+				console.log("error occured : " + error);
+			})
+		}
+		else
+		{
+			Axios.get("leave").then(
+				response => {
+					setAtHome(false);
+					setHome(response.data);
+				}
+			).catch(error => {
+				console.log("error occured : " + error);
+			})
+		}
+	}
+
+	useEffect (() => {
+		/*
+		here can be define the parsing of the name the user has set
+		*/
+		Axios.get(userName).then(
+			response => {
+				setHelloMsg(response.data);
+			}
+		).catch(error => {
+			console.log("error occured : " + error);
+		})
+	}, [userName])
+
+	const changeUserName = () => {
+		setUserName(buildingUserName);
+	}
+
+	const buildUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setBuildingUserName(event.target.value);
+	}
+
 	return (
-		<div>{hello} + pouet</div>)
+		<div>
+			<div>{hello} + pouet</div>
+			<button onClick={homing}>{home}</button>
+			<form onSubmit={handleSubmit(changeUserName)}>
+				<h2>set your name</h2>
+				<input value={buildingUserName}
+					type="text"
+					onChange={buildUserName}
+				/>
+				<button type="submit">SUBMIT</button>
+			</form>
+			{userName !== "" ? <p> {helloMsg} </p> : null}
+		</div>)
 }
 
 export default App
