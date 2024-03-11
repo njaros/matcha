6481deca89d61f5528c4cdd27d8eb5795b9d8a6f
Nicolas Jaros <njaros@z3r8p5.42lyon.fr>
2024-03-11 13:17:06 +0100

    devopsing : psycopg2 -> psycopg3

--- back/api.py
+++ back/api.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource
 from flask_cors import CORS
-from markupsafe import escape
-import psycopg2
+import psycopg
 import os
 
 
@@ -14,10 +13,11 @@ def main():
             {os.environ.get('POSTGRES_IP')=},\
             {os.environ.get('POSTGRES_USER')=},\
             {os.environ.get('POSTGRES_PASSWORD')=}")
-    conn = psycopg2.connect(dbname=os.environ.get("POSTGRES_DB"),
-                            host=os.environ.get("POSTGRES_IP"),
-                            user=os.environ.get("POSTGRES_USER"),
-                            password=os.environ.get("POSTGRES_PASSWORD"))
+    conn = psycopg.connect(
+        dbname=os.environ.get("POSTGRES_DB"),
+        host=os.environ.get("POSTGRES_IP"),
+        user=os.environ.get("POSTGRES_USER"),
+        password=os.environ.get("POSTGRES_PASSWORD"))
     cur = conn.cursor()
     cur.execute("select relname from pg_class where relkind='r'\
         and relname !~ '^(pg_|sql_)';")
@@ -35,7 +35,7 @@ def main():
     api.add_resource(HelloWorld, "/helloworld")
     api.add_resource(Home, "/")
     api.add_resource(Leave, "/leave")
-    api.add_resource(Name, "/<name>")
+    api.add_resource(Name, "/<string:name>")
     app.run(debug=True, host='0.0.0.0', port=port)
 
 
@@ -55,8 +55,8 @@ class Leave(Resource):
 
 
 class Name(Resource):
-    def get(self):
-        return f"Hello, {escape(Resource)}"
+    def get(self, name):
+        return f"Hello, {name}"
 
 
 if __name__ == "__main__":
