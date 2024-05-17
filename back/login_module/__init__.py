88d18e5141fa7088674d86fd813174b4a5ab7f6a
Nicolas Jaros <njaros@z1r7p5.42lyon.fr>
2024-05-17 15:15:44 +0200

    filter page v1

--- back/login_module/__init__.py
+++ back/login_module/__init__.py
@@ -4,9 +4,13 @@ from .routes import login, sign
 from error_status.error import *
 
 
-app = Blueprint('login', __name__)
-CORS(app, supports_credentials=True, expose_headers='Access-Control-Allow-Origin')
+app = Blueprint("login", __name__)
+CORS(
+    app,
+    supports_credentials=True,
+    expose_headers="Access-Control-Allow-Origin",
+)
 
 
-app.add_url_rule('/login', 'login', login, methods=['POST'])
-app.add_url_rule('/sign', 'sign', sign, methods=['POST'])
+app.add_url_rule("/login", "login", login, methods=["POST"])
+app.add_url_rule("/sign", "sign", sign, methods=["POST"])
--- front/src/components/LayoutModules/Layout.tsx
+++ front/src/components/LayoutModules/Layout.tsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import { Box } from "@chakra-ui/react"
 //Ici qu'il faudra mettre les modules du genre pop-ups qui doivent pop de n importe ou
+
 const Layout = (props: {
     logged: boolean,
     handleLog: (newState: boolean) => void,
