74a760b917151e6f85dcd634825bb448366b4acc
pouet <git@mesoule.tamere>
2024-06-07 08:28:41 +0200

    mail doesn't work with mobile bluetooth access...

--- back/mail_config.py
+++ back/mail_config.py
@@ -8,7 +8,7 @@ class Config:
     MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get("MAIL_NAME_ACC")
     MAIL_PASSWORD = os.environ.get("MAIL_APP_PASSWORD")
-    MAIL_DEBUG = False
+    MAIL_DEBUG = True
     MAIL_SUPPRESS_SEND = False
     MAIL_DEFAULT_SENDER = ("Matcha Mailer", "donotresponse@matcha.com")
     MAIL_MAX_EMAILS = None
