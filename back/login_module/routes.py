f04b544447de26d0d4398512aa4c0585a1b46910
pouet <git@mesoule.tamere>
2024-06-22 10:34:55 +0200

    use flask mail

--- back/login_module/routes.py
+++ back/login_module/routes.py
@@ -6,26 +6,22 @@ from user_module import sql as user_ctx
 from error_status.error import BadRequestError
 from tools import GPS_tools
 from . import dto
-from tools.email_tools import Mailer, send_email_register_token, send_reset_password
+from tools.email_tools import send_email_register_token, send_reset_password
 
 
 @dto.signup_dto
 def sign(**kwargs):
     if user_ctx.get_user_by_email(kwargs["email"]) is not None:
         raise (BadRequestError("email already exists"))
-    kwargs["password"] = hashlib.sha256(
-        kwargs["password"].encode("utf-8")
-    ).hexdigest()
+    kwargs["password"] = hashlib.sha256(kwargs["password"].encode("utf-8")).hexdigest()
     kwargs["id"] = login_ctx.insert_new_user_in_database(kwargs)[0].hex
-    Mailer().send_email_register_token(**kwargs)
+    send_email_register_token(**kwargs)
     return [], 201
 
 
 @dto.login_dto
 def login(**kwargs):
-    kwargs["password"] = hashlib.sha256(
-        kwargs["password"].encode("utf-8")
-    ).hexdigest()
+    kwargs["password"] = hashlib.sha256(kwargs["password"].encode("utf-8")).hexdigest()
     if login_ctx.login_user_in_database(kwargs) == 1:
         response = make_response()
         response.status = 403
@@ -33,9 +29,7 @@ def login(**kwargs):
         return response
     if kwargs["user"] is not None:
         current_app.logger.info(kwargs)
-        if (kwargs["latitude"] is None) and (
-            kwargs["user"]["gpsfixed"] is False
-        ):
+        if (kwargs["latitude"] is None) and (kwargs["user"]["gpsfixed"] is False):
             ip_loc = GPS_tools.get_gps_from_ip(request.remote_addr)
             current_app.logger.info(ip_loc)
             if ip_loc is not None:
@@ -44,12 +38,10 @@ def login(**kwargs):
                 login_ctx.update_gps_loc_by_id(**kwargs)
         response = make_response(
             {
-                "access_token": jwt_policy.create_access_token(
-                    kwargs["user"]["id"]
-                ),
+                "access_token": jwt_policy.create_access_token(kwargs["user"]["id"]),
                 "latitude": kwargs["user"]["latitude"],
                 "longitude": kwargs["user"]["longitude"],
-                "gpsfixed": kwargs["user"]["gpsfixed"]
+                "gpsfixed": kwargs["user"]["gpsfixed"],
             }
         )
         response.set_cookie(
@@ -76,14 +68,12 @@ def mail_register(**kwargs):
 
 @dto.reset_password_dto
 def reset_password(**kwargs):
-    Mailer().send_reset_password(**kwargs)
+    send_reset_password(**kwargs)
     return "email sent", 200
 
 
 @dto.confirm_reset_password_dto
 def confirm_reset_password(**kwargs):
-    kwargs["new_pass"] = hashlib.sha256(
-        kwargs["new_pass"].encode("utf-8")
-    ).hexdigest()
+    kwargs["new_pass"] = hashlib.sha256(kwargs["new_pass"].encode("utf-8")).hexdigest()
     login_ctx.update_password_by_email(**kwargs)
     return "<h1>Yeah ! Your password has be successfully reset</h1>", 200
